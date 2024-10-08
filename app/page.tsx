e28abf1377a3e8ba91478e546bb35c4a798562da
coderdal <agircoder@gmail.com>
2024-10-08 22:38:28 +0300

    temporary navigation links added

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div>
-      Hello World
+    <div className="p-10">
+      <nav className="flex flex-col gap-2">
+        <Link href="/remove-pdf-pages" className="bg-blue-500 text-white p-2 rounded-md">Remove PDF Pages</Link>
+        <Link href="/rotate-pdf" className="bg-blue-500 text-white p-2 rounded-md">Rotate PDF Files</Link>
+        <Link href="/extract-pdf-text" className="bg-blue-500 text-white p-2 rounded-md">Extract PDF Text</Link>
+        <Link href="/merge-pdf" className="bg-blue-500 text-white p-2 rounded-md">Merge PDF Files</Link>
+        <Link href="/add-page-number" className="bg-blue-500 text-white p-2 rounded-md">Add Page Numbers</Link>
+      </nav>
     </div>
   );
 }
